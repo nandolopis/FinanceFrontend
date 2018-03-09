ea10e6c0515416d35e21f44f8627f97d39c8db57
Fernando Lopes <nandolopis@gmail.com>
2018-03-09 11:46:09 -0300

    Refatorando para padrao Angular1 Style Guide - controllerAs

--- app/dashboard/dashboardController.js
+++ app/dashboard/dashboardController.js
@@ -1,22 +1,22 @@
 (function(){
     angular.module('primeiraApp').controller('DashboardCtrl', [
-        '$scope',
         '$http',
         DashboardController
     ])
     
-    function DashboardController($scope, $http){
-        $scope.getSummary = function() {
+    function DashboardController($http){
+        const vm = this
+        vm.getSummary = function() {
             const url = 'http://localhost:3003/api/billingSummary'
             $http.get(url).then(function(response){
                 const {credit = 0, debt = 0} = response.data
-                $scope.credit = credit
-                $scope.debt = debt
-                $scope.total = credit - debt
+                vm.credit = credit
+                vm.debt = debt
+                vm.total = credit - debt
             })
         }
     
-        $scope.getSummary()
+        vm.getSummary()
     }
 })()
 
